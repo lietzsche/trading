fc187a809d19a66c9a7dd90ff9b4e85e790408e6
bion0625 <stxtory@gmail.com>
2026-09-09 07:34:02 +0900

    Finish service consolidation hardening

--- trade-service/src/test/java/com/uj/stxtory/StxtoryApplicationTests.java
+++ trade-service/src/test/java/com/uj/stxtory/StxtoryApplicationTests.java
@@ -1,28 +1,13 @@
 package com.uj.stxtory;
 
-import com.uj.stxtory.domain.dto.deal.DealItem;
-import com.uj.stxtory.domain.dto.deal.DealPrice;
-import com.uj.stxtory.domain.dto.stock.StockModel;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class StxtoryApplicationTests {
 
   @Test
   void contextLoads() {}
-
-  @Test
-  void getStockInfo() {
-    StockModel model = new StockModel(130);
-
-    Map<String, List<DealPrice>> pricesMap =
-        model.getAll().stream()
-            //            .limit(5)
-            .collect(Collectors.toMap(DealItem::getName, item -> model.getPrice(item, 130)));
-    System.out.println(pricesMap);
-  }
 }
